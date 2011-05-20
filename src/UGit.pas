3ec1452703f6790b55e49c73d2a79f02be868f32
Giuseppe Monteleone <info@ing-monteleone.com>
2011-05-20 20:32:22 +0200

    FIX: solved a problem with 'fetch' cmd
    Signed-off-by: Giuseppe Monteleone <info@ing-monteleone.com>

--- src/UGit.pas
+++ src/UGit.pas
@@ -578,9 +578,9 @@ end;
 procedure TCRGitInterface.PullRepo(ARepo: string);
 begin
   // we pull all object from remote repo
-  executeCommand(Format('git.exe pull  "%s"',[ARepo]));
+  executeCommand(Format('git.exe pull %s',[ARepo]));
   // now we fetch also tags objects
-  executeCommand(Format('git.exe fetch --tags  "%s"',[ARepo]));
+  executeCommand(Format('git.exe fetch --tags %s',[ARepo]));
 end;
 
 procedure TCRGitInterface.InitRepo;
